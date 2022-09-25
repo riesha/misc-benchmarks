1a7eee6cfc8491235e16d07226dba7d1790ba37b
riesha <seleneia@outlook.jp>
2022-09-25 15:34:46 +0200

    make bcrypt bench more accurate to real world use

--- benches/bcrypt_benchmark.rs
+++ benches/bcrypt_benchmark.rs
@@ -1,6 +1,7 @@
 use bcrypt::DEFAULT_COST;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use rand::{distributions::Alphanumeric, thread_rng, Rng};
+
 pub async fn hash_password(password: String) -> String
 {
     let (send, recv) = tokio::sync::oneshot::channel();
@@ -10,18 +11,27 @@ pub async fn hash_password(password: String) -> String
     });
     recv.await.unwrap().unwrap()
 }
-
+pub async fn verify_password(password: String, hash: String) -> bool
+{
+    let (send, recv) = tokio::sync::oneshot::channel();
+    rayon::spawn(move || {
+        let result = bcrypt::verify(password, &hash);
+        let _ = send.send(result);
+    });
+    recv.await.unwrap().unwrap()
+}
 fn criterion_benchmark(c: &mut Criterion)
 {
-    c.bench_function("bcrypt 12 cost 15char password hashing", |b| {
-        b.iter(|| {
-            hash_password(black_box(
-                thread_rng()
-                    .sample_iter(&Alphanumeric)
-                    .take(15)
-                    .map(char::from)
-                    .collect(),
-            ))
+    c.bench_function("bcrypt 12 cost 15char password hashing and verif", |b| {
+        b.iter(|| async {
+            let rstring: String = thread_rng()
+                .sample_iter(&Alphanumeric)
+                .take(15)
+                .map(char::from)
+                .collect();
+
+            let pass = hash_password(black_box(rstring.to_owned())).await;
+            let _ = verify_password(rstring, pass).await;
         })
     });
 }
